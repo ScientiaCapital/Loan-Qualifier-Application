1ab4b352809f3566f8cdbb06636944ea78563742
Tim Kipper <tkipper@gmail.com>
2021-01-17 09:55:30 -0700

    Upload of changes

--- tests/test_qualifier.py
+++ tests/test_qualifier.py
@@ -1,6 +1,9 @@
 # Import pathlib
-from app import load_bank_data
+from filters.max_loan_size import filter_max_loan_size
 from pathlib import Path
+import csv
+from qualifier.utils.calculators import calculate_monthly_debt_ratio
+import pytest
 
 #Import fileio
 from qualifier.utils import fileio
@@ -9,18 +12,18 @@ from qualifier.utils import fileio
 from qualifier.utils import calculators
 
 # Import Filters
-from qualifier.filters import credit_score
-from qualifier.filters import debt_to_income
-from qualifier.filters import loan_to_value
-from qualifier.filters import max_loan_size
+from qualifier.filters.credit_score import filter_credit_score
+from qualifier.filters.debt_to_income import filter_debt_to_income
+from qualifier.filters.loan_to_value import filter_loan_to_value
+from qualifier.filters.max_loan_size import filter_max_loan_size
 
 def test_save_csv():
     # @TODO: Your code here!
     # Use Path from pathlib to output the test csv to ./data/output/qualifying_loans.csv
-    csv_file = Path("../data/daily_rate_sheet.csv")
-    header = ["A", "b", "c"]
-    qualifying_loans = ['1', '2', '3']
-    save_csv(csv_file, header, qualifying_loans)
+    csv_file = Path("../data/qualifying_loans.csv")
+    bank_data = []
+    header = []
+    fileio.save_csv(csvpath, bank_data, header)
     assert csv_file.exists()
 
 def test_calculate_monthly_debt_ratio():
@@ -30,7 +33,8 @@ def test_calculate_loan_to_value_ratio():
     assert calculators.calculate_loan_to_value_ratio(210000, 250000) == 0.84
 
 def test_filters():
-    bank_data = fileio.load_csv(Path('../data/daily_rate_sheet.csv'))
+    csvpath = Path('../data/daily_rate_sheet.csv'))
+    bank_data = fileio.load_csv((csvpath)
     current_credit_score = 750
     debt = 1500
     income = 4000
@@ -40,7 +44,7 @@ def test_filters():
     loan_to_value_ratio = 0.84
 
     # @TODO: Test the new save_csv code!
-banks = max_loan_size.filter_max_loan_size(loan_to_value, load_bank_data)
+banks = filter_max_loan_size(loan, bank_data)
 print(len(banks))
 assert len(banks) == 18
 current_credit_score = credit_score.filter_credit_score(current_credit_score, banks)
