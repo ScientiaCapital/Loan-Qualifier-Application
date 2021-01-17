b79eb58eb5b31103d2a2cfa638038d62258dc40c
Tim Kipper <tkipper@gmail.com>
2021-01-17 12:35:23 -0700

    Upload changes from teacher

--- app.py
+++ app.py
@@ -15,21 +15,21 @@ import csv
 This will import the necessary libraries that will allow for proper functionality of app.py
 """
 
-from qualifier.utils.fileio import load_csv
+from utils.fileio import load_csv
 
-from qualifier.utils.fileio import save_csv
+from utils.fileio import save_csv
 
-from qualifier.utils.calculators import (
+from utils.calculators import (
     calculate_monthly_debt_ratio,
     calculate_loan_to_value_ratio,
 )
 """
 Imports the qualifiers functions from file within app.py 
 """
-from qualifier.filters.max_loan_size import filter_max_loan_size
-from qualifier.filters.credit_score import filter_credit_score
-from qualifier.filters.debt_to_income import filter_debt_to_income
-from qualifier.filters.loan_to_value import filter_loan_to_value
+from filters.max_loan_size import filter_max_loan_size
+from filters.credit_score import filter_credit_score
+from filters.debt_to_income import filter_debt_to_income
+from filters.loan_to_value import filter_loan_to_value
 
 """
 Imports the filtering fuctions from file within app.py
